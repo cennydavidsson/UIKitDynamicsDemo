47aaa906c440df3d4bfff133ec4e9b79e161c82b
Cenny Davidsson <cenny@Cennys-iMac.local>
2014-07-24 18:17:53 +0200

    Initial Commit

---
+++ UIKit Dynamics Demo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UIKit Dynamics Demo
+//
+//  Created by Cenny Davidsson on 2014-07-24.
+//  Copyright (c) 2014 Cenny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UIKit Dynamics Demo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UIKit Dynamics Demo
+//
+//  Created by Cenny Davidsson on 2014-07-24.
+//  Copyright (c) 2014 Cenny. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
