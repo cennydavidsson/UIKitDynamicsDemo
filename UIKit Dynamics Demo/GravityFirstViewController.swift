a016270b01335dec75b6deb5cb967ec4b0fd8014
Cenny Davidsson <cenny@Cennys-iMac.local>
2014-08-03 13:23:24 +0200

    Added realtime device gravity to gravity demo. Made gravity demo squares larger.

--- UIKit Dynamics Demo/GravityFirstViewController.swift
+++ UIKit Dynamics Demo/GravityFirstViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class GravityFirstViewController: UIViewController {
     
@@ -19,9 +20,12 @@ class GravityFirstViewController: UIViewController {
     let collision = UICollisionBehavior()
     let itemBehavior = UIDynamicItemBehavior()
     
+    let CMManager = CMMotionManager()
+    
     @IBOutlet weak var gravityLabel: UILabel!
     @IBOutlet weak var elasticityLabel: UILabel!
     @IBOutlet weak var animatorRefView: UIView!
+    @IBOutlet weak var gravitySlider: UISlider!
     
     // MARK: UIViewController
     
@@ -43,6 +47,35 @@ class GravityFirstViewController: UIViewController {
     }
     
     // MARK: Interaction
+    
+    @IBAction func changeDeviceGravity(sender: UISwitch) {
+        
+        gravitySlider.enabled = !sender.on
+        
+        if sender.on {
+            // A function that conforms to CMDeviceMotionHandler and updates gravity
+            // based on device orientation
+            func gravityUpdated(motion: CMDeviceMotion!, error: NSError!) {
+                if error { println("\(error)") }
+                
+                // Get and apply gravity to behavior
+                let motionGravity : CMAcceleration = motion.gravity
+                let x = CGFloat(motionGravity.x)
+                let y = CGFloat(motionGravity.y)
+                gravity.gravityDirection = CGVectorMake(x, -y)
+            }
+            
+            // Completion handler updates UI so it needs to be on main queue
+            let mainQueue = NSOperationQueue.mainQueue()
+            
+            CMManager.startDeviceMotionUpdatesToQueue(mainQueue, withHandler: gravityUpdated)
+        } else {
+            gravityAngelChanged(gravitySlider)
+            
+            // Stop updating when we dont need it anymore
+            CMManager.stopDeviceMotionUpdates()
+        }
+    }
     
     @IBAction func gravityAngelChanged(sender: UISlider) {
         gravityLabel.text = "Gravity: \(Int(sender.value))°"
@@ -69,7 +102,7 @@ class GravityFirstViewController: UIViewController {
         
         // Make a square
         let square = UIImageView(image: UIImage(named: "Box1.png"))
-        square.frame.size = CGSize(width: 50, height: 50)
+        square.frame.size = CGSize(width: 70, height: 70)
         square.center = sender.locationInView(animatorRefView)
         square.userInteractionEnabled = true    // is false by default in UIImageView
         animatorRefView.addSubview(square)
@@ -91,7 +124,8 @@ class GravityFirstViewController: UIViewController {
         case .Began:
             //
             let locationInButton = gesture.locationInView(gesture.view)
-            let offset = UIOffset(horizontal: locationInButton.x - CGRectGetWidth(gesture.view.frame)/2, vertical: locationInButton.y - CGRectGetHeight(gesture.view.frame)/2)
+            let offset = UIOffset(horizontal: locationInButton.x - CGRectGetWidth(gesture.view.frame)/2,
+                                    vertical: locationInButton.y - CGRectGetHeight(gesture.view.frame)/2)
             //
             attacthment = UIAttachmentBehavior(item: gesture.view, offsetFromCenter: offset, attachedToAnchor: gesture.locationInView(animatorRefView))
             animator.addBehavior(attacthment)
